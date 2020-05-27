4abbdc64fa7f820c1938ef66b9b82703168f3c40
flatironschool <t.benson@live.co.uk>
2020-05-27 17:07:59 +0100

    created new file

--- first-app.js
+++ first-app.js
@@ -1,1 +1,5 @@
-console.log("Hello from your first app")
+// give access to file system
+const fs = require('fs')
+
+// create file synchronously (name, content)
+fs.writeFileSync('hello.txt','Hello from Node.js')
